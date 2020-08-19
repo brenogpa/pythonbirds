12171ad9790c5f88bb6ddf2e2e34d1a50388e316
Breno Abreu <brenogpa@gmail.com>
2020-08-19 11:57:24 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome = None, idade = 25):
         self.idade = idade
         self.nome = nome
@@ -22,3 +24,8 @@ if __name__ == '__main__':
     del luciano.filhos
     print(luciano.__dict__)
     print(breno.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(breno.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(breno.olhos))
